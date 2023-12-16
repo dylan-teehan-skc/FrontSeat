8ebfdefa885a8351c4db9c4adc77b8441acf431e
dylan-teehan-skc <dylan.teehan@gmail.com>
2023-12-16 15:49:05 +0000

    changes

--- src/main/java/LocationHandling/PassengerCount.java
+++ src/main/java/LocationHandling/PassengerCount.java
@@ -1,10 +1,12 @@
 package LocationHandling;
+
 import java.util.Scanner;
 
 
 public class PassengerCount {
     private static int passengers;
-    public void passengerCount(int passengers){
+
+    public void passengerCount(int passengers) {
         this.passengers = passengers;
     }
 
@@ -19,20 +21,10 @@ public class PassengerCount {
     public void Passengers() {
 
         Scanner scanner = new Scanner(System.in);
-        do {
-            System.out.println("How many passengers will be travelling with you");
-            passengers = getPassengers();
-
-            if (passengers< 0 || passengers > 7) {
-                System.out.println("The maximum capacity is 7");
-
-            } else {
-                System.out.println("You selected " + passengers + "number of passengers");
-                Location startLocation = new Location();
-                startLocation.RunLocation();
-            }
-        } while (passengers < 0 || passengers > 7);
-
+        // previous functionality you had done is now implemented into gui
+        System.out.println("You selected " + passengers + "number of passengers");
+        Location startLocation = new Location();
+        startLocation.RunLocation();
 
 
     }
